7aff5c2332a62de54444d8f1bba24bd1f77099d8
Matheus Monteiro <matheus.stack.20@gmail.com>
2022-05-01 21:46:59 -0300

    Implementação de novas rotas, nomeação das rotas e implementação da rota de fallback

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get('/login', function () {
+    return "Login";
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return "Clientes";
+    })->name('app.clientes');
+
+    Route::get('/fornecedores', function () {
+        return "Fornecedores";
+    })->name('app.fornecedores');
+
+    Route::get('/produtos', function () {
+        return "Produtos";
+    })->name('app.produtos');
+});
+
+Route::fallback(function () {
+    return 'Página não encontrada, Volte para a página inicial <a href="' . route('site.index') . '"> voltar </a>';
+})->name('site.404');
